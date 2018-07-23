cb72fbd7c8011cdb51e20ccd1c0ba20d9982f622
root <root@demo-ubuntu.4qobuos5pzuu3cklqgtomupmhh.bx.internal.cloudapp.net>
2018-07-23 17:24:16 +0000

    updated network module

--- networking/varibales.tf
+++ networking/varibales.tf
@@ -3,3 +3,16 @@
 variable "vpc_cidr" {
 default = "10.123.0.0/16"
 }
+
+variable "public_cidrs" {
+ default = [
+  "10.123.1.0/24",
+  "10.123.2.0/24"
+ ]
+}
+
+variable "accessip" {
+
+default = "0.0.0.0./0"
+
+}

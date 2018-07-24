8c567e7833d90173caadf3d7f010a3cf4563446a
root <root@demo-ubuntu.4qobuos5pzuu3cklqgtomupmhh.bx.internal.cloudapp.net>
2018-07-24 12:26:31 +0000

    network module with main module

--- main.tf
+++ main.tf
@@ -5,9 +5,20 @@ provider "aws" {
 
 # deploy Storage resources
 module "storage" {
+ source = "./storage"
+ project_name = "${var.project_name}"
+}
 
-source = "./storage"
-project_name = "${var.project_name}"
+
+# Deploy networking resources 
+
+module "networking" {
+ source = "./networking"
+ vpc_cidr = "${var.vpc_cidr}"
+ public_cidrs = "${var.public_cidrs}"
+ accessip = "${var.accessip}"
+
+
 }
 
 
--- networking/varibales.tf
+++ networking/varibales.tf
@@ -1,18 +1,12 @@
 #-----------------------networking/variable.tf-------------------------------
 
 variable "vpc_cidr" {
-default = "10.123.0.0/16"
+
 }
 
 variable "public_cidrs" {
- default = [
-  "10.123.1.0/24",
-  "10.123.2.0/24"
- ]
+ type = "list"
 }
 
 variable "accessip" {
-
-default = "0.0.0.0/0"
-
 }
